fe378fbab9d7361732e30f2976b3e8455657c695
Kelli Tamashiro <kellikt@hawaii.edu>
2020-06-15 17:21:55 -1000

    Idealist scraper works (big ty to Jenny!!)

--- idealist.js
+++ idealist.js
@@ -21,35 +21,65 @@ async function writeData(data) {
           console.log('\nData successfully written!')));
 }
 
-async function getElements(page) {
-  const elements = await page.evaluate(
+async function getLinks(page) {
+  const links = await page.evaluate(
       () => Array.from(
           // eslint-disable-next-line no-undef
           document.querySelectorAll('[data-qa-id=search-result-link]'),
-          a => a.getAttribute('href'),
+      a => a.getAttribute('href'),
       ),
   );
-  return elements
+  return links;
+}
+
+async function getElements(page) {
+  let hasNext = true;
+  const elements = [];
+  while (hasNext == true) {
+    try {
+      await page.waitFor(1000);
+      getLinks(page).then(links => {
+        elements.push(links);
+      })
+      await page.waitForSelector('button[class="Button__StyledButton-sc-1avp0bd-0 ggDAbQ Pagination__ArrowLink-nuwudv-2 eJsmUe"]:last-child');
+      nextPage = await page.$('button[class="Button__StyledButton-sc-1avp0bd-0 ggDAbQ Pagination__ArrowLink-nuwudv-2 eJsmUe"]:last-child');
+      await nextPage.click();
+      //console.log(elements);
+    } catch(e) {
+      console.log(e.message);
+      console.log(elements);
+      hasNext = false;
+      console.log('\nReached the end of pages!');
+    }
+  }
+  console.log(elements);
+  return elements;
 }
 
 async function getData(page, elements) {
-  const data = [];
-  for (let i = 0; i < elements.length; i++) {
-    const element = 'https://www.idealist.org' + elements[i];
-    console.log(element);
-    await page.goto(element, { waitUntil: 'domcontentloaded' });
-    const position = await fetchInfo(page, '[data-qa-id=listing-name]');
-    const company = await fetchInfo(page, '[data-qa-id=org-link]');
-    const description = await fetchInfo(page, '.Text-sc-1wv914u-0.dlxdi.idlst-rchtxt.Text__StyledRichText-sc-1wv914u-1.ctyuXi');
-    console.log(position);
-    data.push({
-      position: position,
-      company: company,
-      description: description,
-      currentURL: element,
-    })
+  try {
+    const data = [];
+    for (let i = 0; i < elements.length; i++) {
+      for (let j = 0; j < elements[i].length; j++) {
+        const element = 'https://www.idealist.org' + elements[i][j];
+        console.log(element);
+        await page.goto(element, { waitUntil: 'domcontentloaded' });
+        const position = await fetchInfo(page, '[data-qa-id=listing-name]');
+        const company = await fetchInfo(page, '[data-qa-id=org-link]');
+        const description = await fetchInfo(page, '.Text-sc-1wv914u-0.dlxdi.idlst-rchtxt.Text__StyledRichText-sc-1wv914u-1.ctyuXi');
+        console.log(position);
+        data.push({
+          position: position,
+          company: company,
+          description: description,
+          currentURL: element,
+        })
+      }
+    }
+    return data;
+  } catch(e) {
+    console.log(e.message);
   }
-  return data;
 }
 
 (async () => {
@@ -62,8 +92,8 @@ async function getData(page, elements) {
 
     await page.goto('https://www.idealist.org/en/');
     await page.waitForSelector('#layout-root > div.idlst-flx.Box__BaseBox-sc-1wooqli-0.lnKqQM > div.idlst-flx.Box__BaseBox-sc-1wooqli-0.dCQmbn.BaseLayout__PageContent-sc-10xtgtb-2.heQjSt > div.Box__BaseBox-sc-1wooqli-0.bsSECh > div > div.Box__BaseBox-sc-1wooqli-0.hpEILX > div.Box__BaseBox-sc-1wooqli-0.datyjK > div > div > div.idlst-flx.idlst-lgncntr.Box__BaseBox-sc-1wooqli-0.cDmdoN > div > form > div.Box__BaseBox-sc-1wooqli-0.ejycyy > div > input');
-    await page.$eval('#layout-root > div.idlst-flx.Box__BaseBox-sc-1wooqli-0.lnKqQM > div.idlst-flx.Box__BaseBox-sc-1wooqli-0.dCQmbn.BaseLayout__PageContent-sc-10xtgtb-2.heQjSt > div.Box__BaseBox-sc-1wooqli-0.bsSECh > div > div.Box__BaseBox-sc-1wooqli-0.hpEILX > div.Box__BaseBox-sc-1wooqli-0.datyjK > div > div > div.idlst-flx.idlst-lgncntr.Box__BaseBox-sc-1wooqli-0.cDmdoN > div > form > div.Box__BaseBox-sc-1wooqli-0.ejycyy > div > input', (el, text) => el.value = text, 'Computer Science');
-    await page.click('#layout-root > div.idlst-flx.Box__BaseBox-sc-1wooqli-0.lnKqQM > div.idlst-flx.Box__BaseBox-sc-1wooqli-0.dCQmbn.BaseLayout__PageContent-sc-10xtgtb-2.heQjSt > div.Box__BaseBox-sc-1wooqli-0.bsSECh > div > div.Box__BaseBox-sc-1wooqli-0.hpEILX > div.Box__BaseBox-sc-1wooqli-0.datyjK > div > div > div.idlst-flx.idlst-lgncntr.Box__BaseBox-sc-1wooqli-0.cDmdoN > div > form > div.Box__BaseBox-sc-1wooqli-0.ibeatg > button');
+    await page.type('input[data-qa-id="search-input"]', 'computer science');
+    await page.click('button[data-qa-id="search-button"]');
     await page.waitForSelector('#results > div > div > div.Box__BaseBox-sc-1wooqli-0.iuHlOF > div:nth-child(2) > div > div > div > div.Box__BaseBox-sc-1wooqli-0.csFszx > div.Box__BaseBox-sc-1wooqli-0.iKEEgc > h4 > a');
 
     //let hasNext = true;
@@ -100,12 +130,14 @@ async function getData(page, elements) {
     console.log(elements);
     */
 
-    getElements(page).then((elements) => {
+    await getElements(page).then((elements) => {
       getData(page, elements).then((data => {
         console.log(data);
         writeData(data);
       }))
     })
+
+    // await browser.close();
 /*
     for (let i = 0; i < elements.length; i++) {
       const element = 'https://www.idealist.org' + elements[i];
--- soc.js
+++ soc.js
@@ -1,11 +1,25 @@
 const puppeteer = require('puppeteer-extra');
-const USERNAME_SELECTOR = '#mat-input-0';
-const PASSWORD_SELECTOR = '#mat-input-1';
+const fs = require('fs');
+const USERNAME_SELECTOR = 'input[aria-label="email"]';
+const PASSWORD_SELECTOR = 'input[aria-label="password"]';
 const CTA_SELECTOR = '#login-submit-button';
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 
 const credentials = process.argv.slice(2);
 const userAgent = require('user-agents');
+
+async function fetchInfo(page, selector) {
+  let result = '';
+  try {
+
+    await page.waitForSelector(selector);
+    result = await page.evaluate((select) => document.querySelector(select).textContent, selector);
+  } catch (error) {
+    console.log('Our Error: fetchInfo() failed.\n', error.message);
+    result = 'Error';
+  }
+  return result;
+}
 
 async function fetchInfo(page, selector) {
   let result = '';
@@ -17,6 +31,24 @@ async function fetchInfo(page, selector) {
     result = 'Error';
   }
   return result;
+}
+
+async function getLinks(page) {
+  const links = await page.evaluate(
+      () => Array.from(
+          // eslint-disable-next-line no-undef
+          document.querySelectorAll('a[_ngcontent-c42]'),
+          a => a.getAttribute('href'),
+      ),
+  );
+  return links;
+}
+
+async function writeData(data) {
+  await fs.writeFile('./soc.data.json',
+      JSON.stringify(data, null, 4), 'utf-8',
+      err => (err ? console.log('\nData not written!', err) :
+          console.log('\nData successfully written!')));
 }
 
 async function startBrowser() {
@@ -35,6 +67,8 @@ async function login(url, browser, page) {
   page.setViewport({width: 1366, height: 768});
   await page.setUserAgent('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
   await page.goto(url);
+  await page.waitForSelector(USERNAME_SELECTOR);
+  await page.waitForSelector(PASSWORD_SELECTOR);
   await page.click(USERNAME_SELECTOR);
   await page.keyboard.type(credentials[0]);
   await page.waitFor(20000)
@@ -45,12 +79,35 @@ async function login(url, browser, page) {
 }
 
 async function input(page) {
-  await page.waitForSelector('#mat-input-2', {
+  await page.waitForSelector('input[aria-label="Search Bar"]', {
     timeout: 100000
   });
-  await page.click('#mat-input-2');
-  await page.keyboard.type('computer science');
+  await page.click('input[aria-label="Search Bar"]');
+  await page.$eval('input[aria-label="Search Bar"]', (el) => el.value = 'computer science hawaii');
+  await (await page.$('input[aria-label="Search Bar"]')).press('Enter');
   await page.screenshot({path: 'soc.png'});
+}
+
+async function getData(page, elements) {
+  try {
+    const data = [];
+    for (let i = 0; i < elements.length; i++) {
+        const element = 'https://app.studentopportunitycenter.com/' + elements[i];
+        console.log(element);
+        await page.goto(element);
+        const position = await fetchInfo(page, 'span[_ngcontent-c62]');
+        const description = await fetchInfo(page, 'span[class="pb-8 mat-body-1 wrap-text"]');
+        console.log(position);
+        data.push({
+          position: position,
+          description: description,
+          currentURL: element,
+        })
+    }
+    return data;
+  } catch(e) {
+    console.log(e.message);
+  }
 }
 
 (async () => {
@@ -58,4 +115,11 @@ async function input(page) {
   const {browser, page} = await startBrowser();
   await login("https://app.studentopportunitycenter.com/app/search", browser, page);
   await input(page);
+  await getLinks(page).then((links => {
+    console.log(links);
+    getData(page, links).then((data => {
+      console.log(data);
+      writeData(data);
+    }))
+  }))
 })();
