3b583f4716483813ce0bb7e5e5395120774f9f2f
Kelli Tamashiro <kellikt@hawaii.edu>
2020-06-22 11:29:31 -1000

    testing nlp stuff, tokenizing

---
+++ naturalTest.js
@@ -0,0 +1,30 @@
+const natural = require('natural');
+const fs = require('fs');
+
+const tokenizer = new natural.WordTokenizer();
+const ideaFile = './idealist.canonical.data.json';
+
+const text = fs.readFileSync(ideaFile).toString('utf-8');
+// eslint-disable-next-line no-eval
+const arr = eval(`[${text}]`);
+console.log(arr[0][1]);
+console.log(tokenizer.tokenize(arr[0][1].description));
+/*
+async function read(filename) {
+  fs.readFile(filename, 'utf8', function (err, fileContents) {
+    if (err) throw err;
+    ideaTest = JSON.parse(fileContents);
+  });
+}
+(async () => {
+  await read(ideaFile);
+  await console.log(ideaTest);
+ */
+  /*
+  for (let i = 0; i < ideaTest.length; i++) {
+    // eslint-disable-next-line no-console
+    console.log(ideaTest[i]);
+    console.log(tokenizer.tokenize(ideaTest[i].description));
+  }
+   */
+// })();
